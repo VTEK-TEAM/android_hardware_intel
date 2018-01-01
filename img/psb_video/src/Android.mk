LineageOS/android_hardware_intel_img_psb_video

intel: psb_video: Add support for LP blobs
The gralloc interface has changed to add a new method which was
previously magically buried in a "reserved" pointer.  Restore the
LP version of the code to support our older blobs.

Change-Id: Ib4f812a6d0efdcc528fa35ec0b1dc38ae149d1cb

src/Android.mk
@@ -160,6 +160,10 @@ LOCAL_CFLAGS += \
     -DPSBVIDEO_MSVDX_DEC_TILING -DPSBVIDEO_MSVDX_EC
 endif
 
+ifeq ($(INTEL_HWC_MOOREFIELD_HAS_LP_BLOBS),true)
+    LOCAL_CFLAGS += -DHAS_LP_BLOBS
+endif
+
 ifeq ($(TARGET_HAS_MULTIPLE_DISPLAY),true)
 LOCAL_SRC_FILES += android/psb_mds.cpp
 LOCAL_CFLAGS += -DTARGET_HAS_MULTIPLE_DISPLAY

src/android/psb_gralloc.cpp
@@ -145,7 +145,16 @@ int gralloc_init(void)
 int gralloc_getdisplaystatus(buffer_handle_t handle,  int* status)
 {
     int err;
-#ifndef BAYTRAIL
+#ifdef HAS_LP_BLOBS
+    int (*get_display_status)(gralloc_module_t*, buffer_handle_t, int*);
+
+    get_display_status = (int (*)(gralloc_module_t*, buffer_handle_t, int*))(mAllocMod->reserved_proc[0]);
+    if (get_display_status == NULL) {
+        ALOGE("can't get gralloc_getdisplaystatus(...) \n");
+        return -1;
+    }
+    err = (*get_display_status)(mAllocMod, handle, status);
+#elif defined(BAYTRAIL)
     uint32_t _status = 0U;
     err = mAllocMod->perform(mAllocMod, GRALLOC_MODULE_GET_DISPLAY_STATUS_IMG, handle, &_status);
     *status = (int)_status;

VTEK-TEAM/android_hardware_intel

img/psb_video/src/Android.mk
@@ -166,6 +166,10 @@ LOCAL_CFLAGS += \
     -DPSBVIDEO_MSVDX_DEC_TILING -DPSBVIDEO_MSVDX_EC
 endif
 
+ifeq ($(INTEL_HWC_MOOREFIELD_HAS_LP_BLOBS),true)
+    LOCAL_CFLAGS += -DLP_BLOBS
+endif
+
 ifeq ($(TARGET_HAS_MULTIPLE_DISPLAY),true)
 LOCAL_SRC_FILES += android/psb_mds.cpp
 LOCAL_CFLAGS += -DTARGET_HAS_MULTIPLE_DISPLAY

img/psb_video/src/android/psb_gralloc.cpp
@@ -198,6 +198,15 @@ int gralloc_getdisplaystatus(buffer_handle_t handle,  int* status)
 #ifdef BAYTRAIL
     *status = mGralloc->perform(mGralloc, INTEL_UFO_GRALLOC_MODULE_PERFORM_GET_BO_STATUS, handle);
     err = 0;
+#elif defined(LP_BLOBS)
+    int (*get_display_status)(gralloc_module_t*, buffer_handle_t, int*);
+
+    get_display_status = (int (*)(gralloc_module_t*, buffer_handle_t, int*))(mGralloc->reserved_proc[0]);
+    if (get_display_status == NULL) {
+        ALOGE("can't get gralloc_getdisplaystatus(...) \n");
+        return -1;
+    }
+    err = (*get_display_status)(mGralloc, handle, status);
 #else
     uint32_t _status = 0U;
     err = gralloc_get_display_status_img(mGralloc, handle, &_status);